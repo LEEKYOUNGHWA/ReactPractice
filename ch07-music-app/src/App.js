d816d91afe89770a6d3b579076465893535cdc03
LEEKYOUNGHWA <rudghk5220@gmail.com>
2020-04-16 21:10:24 +0900

    commit

--- ch07-music-app/src/App.js
+++ ch07-music-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Test1 from './components/Test1';
 
 function App() {
   return (
     <div className="App">
-     
+      <Test1/>
     </div>
   );
 }
