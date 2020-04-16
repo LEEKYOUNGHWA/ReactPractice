5b7da6f2f006b698fd52e417771d9467cf267fca
LEEKYOUNGHWA <rudghk5220@gmail.com>
2020-04-16 21:12:59 +0900

    commit

--- ch07-music-app/src/components/Test1.js
+++ ch07-music-app/src/components/Test1.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class Test1 extends Component {
+    state ={
+        count : 0
+    }
+    handleUp = () =>{
+        const {count} = this.state
+        this.setState({
+            count : count+1
+        })
+
+    }
+
     render() {
+        
+
+
         return (
             <div>
-                
+                <h2> </h2>
+                <button onClick={this.handleUp}>증가</button>
             </div>
         );
     }
