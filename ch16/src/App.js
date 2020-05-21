30239ea527eaf7ea836fce93f3cfecc9047b9ee9
LEEKYOUNGHWA <rudghk5220@gmail.com>
2020-05-21 22:26:39 +0900

    cmt

--- ch16/src/App.js
+++ ch16/src/App.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Test8 from './components/Test8';
+import {createStore} from 'redux'
+import {connect, Provider} from 'react-redux'
 
-function App() {
+const Counter = ({value,increment,decrement}) => {
   return (
-    <div >
-      <Test8/>
+    <div>
+      <h1>숫자증가감소 : {value}</h1>
+      <button onClick={increment}>증가</button>
+      <button onClick={decrement}>감소</button>
     </div>
-  );
+  )
+};
+
+// connect, 매핑
+const mapStateToProps = (state) =>{
+  return {
+    value : state.count
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return { 
+    increment : () => dispatch( {type : 'INCREMENT'}),
+    decrement : () => dispatch( {type: 'DECREMENT'})
+  }
 }
 
+const ReduxContainer = connect (
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
+
+//리듀서
+const initState = { count: 0}
+const reducer = (state=initState , action) =>{
+  
+  switch(action.type){
+    case 'INCREMENT' : 
+    return {
+      ...state,
+      count : state.count+1
+    }
+
+    case 'DECREMENT' : 
+    return {
+      ...state,
+      count : state.count -1
+    }
+
+    default : 
+    return state
+  }
+}
+
+const App = () =>{
+  const store = createStore(reducer)
+  return (
+    <Provider store ={store}>
+      <ReduxContainer/>
+    </Provider>
+  )
+}
 export default App;
